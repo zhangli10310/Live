3b9bdaf2e9822cb22c0320511363bf58a93fe97d
zhangli <1031041235@qq.com>
2022-01-12 12:13:05 +0800

    z

--- livepush/src/main/java/com/zl/livepush/PushActivity.kt
+++ livepush/src/main/java/com/zl/livepush/PushActivity.kt
@@ -1,11 +1,19 @@
 package com.zl.livepush
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.core.impl.ImageAnalysisConfig
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.zl.livepush.databinding.ActivityPushBinding
 
 class PushActivity : AppCompatActivity() {
@@ -14,7 +22,7 @@ class PushActivity : AppCompatActivity() {
 
     private lateinit var pushBinding: ActivityPushBinding
 
-    private val requestCameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+    val cameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
         if (it) {
             startPreview()
         } else {
@@ -29,11 +37,48 @@ class PushActivity : AppCompatActivity() {
 
 
         pushBinding.startPush.setOnClickListener {
-            requestCameraPermission.launch(Manifest.permission.CAMERA)
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                startPreview()
+            } else {
+                cameraPermission.launch(Manifest.permission.CAMERA)
+            }
         }
     }
 
     private fun startPreview() {
         Log.i(TAG, "startPreview: ")
+        val providerFuture = ProcessCameraProvider.getInstance(this)
+        providerFuture.addListener({
+            val provider = providerFuture.get()
+            val preview = Preview.Builder()
+                .build()
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            val analysis = ImageAnalysis.Builder().build()
+            analysis.setAnalyzer(ContextCompat.getMainExecutor(this), {
+                push(it)
+                it.close()
+            })
+            try {
+                provider.unbindAll()
+                provider.bindToLifecycle(this, cameraSelector, preview, analysis)
+                preview.setSurfaceProvider(pushBinding.previewView.surfaceProvider)
+//                preview.setSurfaceProvider {
+//                    it.provideSurface()
+//                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun push(imageProxy: ImageProxy) {
+
     }
 }
